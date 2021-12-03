e6f89a34c18bc2aadd56b76986f2e5ea365a2e21
Hidalgo71 <y.ozdemir71@gmail.com>
2021-12-03 14:18:53 +0300

    Updated

--- src/test/java/tr/com/gittigidiyor/page/LogoutPage.java
+++ src/test/java/tr/com/gittigidiyor/page/LogoutPage.java
@@ -1,6 +1,9 @@
 package tr.com.gittigidiyor.page;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import tr.com.gittigidiyor.base.BasePage;
 
 public class LogoutPage extends BasePage
@@ -8,5 +11,17 @@ public class LogoutPage extends BasePage
     public LogoutPage(WebDriver driver)
     {
         super(driver);
+    }
+
+    public void newTab()
+    {
+        String openTab = Keys.chord(Keys.CONTROL, Keys.RETURN);
+        driver.findElement(By.linkText("https://www.gittigidiyor.com/")).sendKeys(openTab);
+    }
+    public void logout()
+    {
+        Actions action = new Actions(driver);
+        action.moveToElement(driver.findElement(By.xpath("//div[@class='gekhq4-5 hJhHqb']//div[@class='gekhq4-6 hnYHyk']"))).build().perform();
+        action.moveToElement(driver.findElement(By.xpath("//a[contains(text(),'Çıkış')]"))).click();
     }
 }
--- src/test/java/tr/com/gittigidiyor/test/CoreTest.java
+++ src/test/java/tr/com/gittigidiyor/test/CoreTest.java
@@ -1,9 +1,6 @@
 package tr.com.gittigidiyor.test;
 
 import org.junit.Test;
-import org.junit.jupiter.api.Order;
-import org.openqa.selenium.WebDriver;
-import tr.com.gittigidiyor.base.BasePage;
 import tr.com.gittigidiyor.base.BaseTest;
 import tr.com.gittigidiyor.page.*;
 
@@ -16,9 +13,9 @@ public class CoreTest extends BaseTest
     RandomAddPage addRandom;
     CartPage cartPage;
     PaymentPage paymentPage;
+    LogoutPage logoutPage;
 
     @Test
-    @Order(1)
     public void loginMethod() throws InterruptedException
     {
         loginPage = new LoginPage(driver);
@@ -27,15 +24,13 @@ public class CoreTest extends BaseTest
     }
 
    @Test
-    @Order(2)
-    public void searchScrollMethod()
-    {
-        searchScroll = new SearchScrollPage(driver);
-        searchScroll.searchScroll();
-    }
+   public void searchScrollMethod()
+   {
+       searchScroll = new SearchScrollPage(driver);
+       searchScroll.searchScroll();
+   }
 
     @Test
-    @Order(3)
     public void addRandomMethod()
     {
         addRandom = new RandomAddPage(driver);
@@ -43,7 +38,6 @@ public class CoreTest extends BaseTest
     }
 
     @Test
-    @Order(4)
     public void bagSearchMethod() throws InterruptedException
     {
         bagSearchAdd = new BagSearchAddPage(driver);
@@ -52,7 +46,6 @@ public class CoreTest extends BaseTest
     }
 
    @Test
-   @Order(5)
     public void cartPageMethod() throws InterruptedException
    {
         cartPage = new CartPage(driver);
@@ -60,11 +53,16 @@ public class CoreTest extends BaseTest
     }
 
     @Test
-    @Order(6)
     public void paymentAddressMethod()
     {
         paymentPage.addressErrors();
         paymentPage.editBasket();
         paymentPage.addFav();
+    }
+    @Test
+    public void logoutMethode()
+    {
+        logoutPage.newTab();
+        logoutPage.logout();
     }
 }
